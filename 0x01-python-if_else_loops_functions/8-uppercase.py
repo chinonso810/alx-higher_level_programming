7dfe253948904b62f71725e51bda30bd128b59f0
root <chinonsoe551@gmail.com>
2024-04-29 22:03:42 -0700

    correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,11 +2,8 @@
 def uppercase(str):
     uppercase_str = ""
     for char in str:
-        # Check if the character is lowercase
         if 'a' <= char <= 'z':
-            # Convert the character to uppercase using ASCII manipulation
             uppercase_char = chr(ord(char) - 32)
-            # Append the uppercase character to the result string
             uppercase_str += uppercase_char
         else:
             uppercase_str += char
